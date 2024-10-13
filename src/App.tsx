a56d5e9836840e4d74bac5c11149671f011399ac
unknown <guillerm.aldana@gmail.com>
2024-10-13 14:52:48 -0300

    feat: implement page to search value from service

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import Title from './components/Title';
+import Input from './components/Input';
+import Table from './components/Table';
+import { INote } from './domain/note';
+import { data, fetchData } from './services/noteServices';
 
 const App = () => {
+  const [note, setNote] = useState<INote[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  const getNoteByLegajo = async (legajo: string) => {
+    setLoading(true);
+    if (legajo) {
+      try {
+        const response = data.filter(element => element.legajo === legajo);
+        setNote(response);
+      } catch (error) {
+        console.error("Ha ocurrido un problema");
+      } finally {
+        setLoading(false);
+      }
+
+    }
+  };
   return (
-    <div className="content">
-      <h1>Rsbuild with React</h1>
-      <p>Start building amazing things with Rsbuild.</p>
+    <div className="container mx-auto h-screen flex flex-col justify-center items-center px-4">
+      <Title text="Buscar notas de examenes" />
+      <Input getNoteByLegajo={getNoteByLegajo} setNote={setNote} />
+      {note.length > 0 && <Table note={note} loading={loading} /> }
     </div>
   );
 };
