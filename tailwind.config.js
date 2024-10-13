bb303041a52b79c905aabd765bbf89f7b62b2831
unknown <guillerm.aldana@gmail.com>
2024-10-13 14:53:08 -0300

    chore: configure file to use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Rubik', 'sans-serif'], // Cambia 'Poppins' por la fuente que quieras
+      },
+    },
+  },
+  plugins: [],
+};
